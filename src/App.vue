09e922f2253cde56bd244816da8ef7838e2fb84e
wariszargardev <wariszargardev@gmail.com>
2022-11-11 11:25:42 +0500

    Dynamic redirect to componsnet

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,39 @@
     <router-link to="/about">About</router-link> |
     <router-link :to="{name: 'BookListingView'}">Books</router-link>
   </nav>
+
+  <button @click="home">Home</button>
+  <button @click="gotoBook">Goto Book 1</button>
+  <button @click="back">Back</button>
+  <button @click="forward">Forward</button>
   <router-view/>
 </template>
 
 <script>
 
+import router from "@/router";
+
 export default {
   name: 'App',
+  methods:{
+    back(){
+      router.back()
+    },
+    forward(){
+      router.forward()
+    },
+    home(){
+      this.$router.push({
+        name: 'home'
+      })
+    },
+    gotoBook(){
+      this.$router.push({
+        name: "BookDetailView",
+        params :{id: 1}
+      })
+    }
+  }
 }
 </script>
 
